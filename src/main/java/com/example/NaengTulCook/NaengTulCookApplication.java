723d18f96b29294df5326327c6fb5df5e415bcba
김나영 <kny200208@naver.com>
2025-02-05 20:02:36 +0900

    Initial commit

---
+++ src/main/java/com/example/NaengTulCook/NaengTulCookApplication.java
@@ -0,0 +1,13 @@
+package com.example.NaengTulCook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaengTulCookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaengTulCookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NaengTulCook/NaengTulCookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NaengTulCook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaengTulCookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
