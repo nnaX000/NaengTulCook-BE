ac4e579962d81667bd9adc52b1184cbcc9c58a45
김나영 <kny200208@naver.com>
2025-02-09 23:40:57 +0900

    feat : swagger 연결

--- src/main/java/com/example/NaengTulCook/controller/UserController.java
+++ src/main/java/com/example/NaengTulCook/controller/UserController.java
@@ -2,6 +2,9 @@ package com.example.NaengTulCook.controller;
 
 import com.example.NaengTulCook.dto.UserSignUpRequest;
 import com.example.NaengTulCook.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,9 +20,16 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Operation(summary = "회원가입", description = "Create a new user and register them to the system")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User registered successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid user data provided"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @PostMapping("/signup")
     public ResponseEntity<String> signUp(@RequestBody UserSignUpRequest request) {
         userService.signUp(request);
+        // 사용자 가입 로직
         return new ResponseEntity<>("User registered successfully", HttpStatus.CREATED);
     }
 }
