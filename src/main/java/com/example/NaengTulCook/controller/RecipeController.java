50f8d7b0b12baea123c94036723a5131906cfee7
김나영 <kny200208@naver.com>
2025-02-12 01:25:50 +0900

    feat : 레시피 랜덤 추천 api

--- src/main/java/com/example/NaengTulCook/controller/RecipeController.java
+++ src/main/java/com/example/NaengTulCook/controller/RecipeController.java
@@ -1,20 +1,22 @@
 package com.example.NaengTulCook.controller;
 
+import com.example.NaengTulCook.entity.User;
+import com.example.NaengTulCook.repository.UserRepository;
 import com.example.NaengTulCook.service.RecipeService;
+import com.example.NaengTulCook.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/recipes")
@@ -22,6 +24,11 @@ public class RecipeController {
 
     @Autowired
     private RecipeService recipeService;
+
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private UserRepository userRepository;
 
     @Operation(summary = "레시피 데이터 업로드(프론트 사용x)", description = "")
     @ApiResponses(value = {
@@ -34,5 +41,11 @@ public class RecipeController {
         recipeService.saveRecipesFromExcel(filePath);
 
         return new ResponseEntity<>("Recipes uploaded successfully", HttpStatus.CREATED);
+    }
+
+    @GetMapping("/recommend")
+    public List<Map<String, Object>> getRecommendedRecipes(@RequestParam int userId) {
+        User user = userRepository.findUsersById(userId);
+        return recipeService.recommendRecipes(user);
     }
 }
