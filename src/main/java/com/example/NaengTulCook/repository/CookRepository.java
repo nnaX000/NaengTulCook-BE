9228fc0c9eb1992461ca7eecc96769114106f95a
김나영 <kny200208@naver.com>
2025-02-23 20:34:19 +0900

    feat : 유저별 기존 필터 설정 조회

--- src/main/java/com/example/NaengTulCook/repository/CookRepository.java
+++ src/main/java/com/example/NaengTulCook/repository/CookRepository.java
@@ -3,5 +3,8 @@ package com.example.NaengTulCook.repository;
 import com.example.NaengTulCook.entity.Cook;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CookRepository extends JpaRepository<Cook, Integer> {
+    Optional<Cook> findTopByUserIdOrderByIdDesc(int userId);
 }
